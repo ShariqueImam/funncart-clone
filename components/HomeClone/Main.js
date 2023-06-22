b65f756fc934dd7f2ad91137a1fd5e7a85cf0d13
Sharique Imam <shariquegithubweb@gmail.com>
2023-06-22 18:03:01 +0500

    adsfg

--- components/HomeClone/Main.js
+++ components/HomeClone/Main.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Home from "./Home";
 import Stats from "./Stats";
-import Features from './Features'
-import Category from './Category'
-import Testimonial from './Testimonial'
+import Features from "./Features";
+import Category from "./Category";
+import Testimonial from "./Testimonial";
+import Trending from "./Trending";
+import Seller from "./Seller";
+import Footer from "./Footer";
 const Main = () => {
   return (
     <div>
@@ -11,7 +14,10 @@ const Main = () => {
       <Stats />
       <Category />
       <Features />
+      <Trending />
       <Testimonial />
+      <Seller />
+      <Footer />
     </div>
   );
 };
