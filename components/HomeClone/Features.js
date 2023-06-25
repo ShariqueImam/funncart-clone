bfd6b1e58e0aacaf3f66b8d0cc009042fe2dd654
uzair1707 <51264502+uzair1707@users.noreply.github.com>
2023-06-25 20:11:32 +0500

    Update Features.js
    Updated to fix alignment of features section

--- components/HomeClone/Features.js
+++ components/HomeClone/Features.js
@@ -10,7 +10,8 @@ const incentives = [
     name: "Swift Delivery",
     description:
       "We ensure seamless and timely delivery, guaranteeing your order reaches your doorstep promptly",
-    imageSrc: "https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg",
   },
   {
     name: "Exquisite Quality",
@@ -32,18 +33,22 @@ export default function Example() {
   return (
     <div className="bg-gray-50">
       <div className="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
-<div className="mx-auto max-w-xl text-center">
+        <div className="mx-auto max-w-xl text-center">
           <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
             Our Features
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-       Why choose us
+            Why choose us
           </p>
         </div>
         <div className="grid grid-cols-1 gap-y-12 mt-16 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
           {incentives.map((incentive) => (
-            <div key={incentive.name}>
-              <img src={incentive.imageSrc} alt="" className="h-24 w-auto" />
+            <div key={incentive.name} className="flex flex-col items-center">
+              <img
+                src={incentive.imageSrc}
+                alt=""
+                className="h-24 w-auto"
+              />
               <h3 className="mt-6 text-sm font-medium text-gray-900">
                 {incentive.name}
               </h3>
