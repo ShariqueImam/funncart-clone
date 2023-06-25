eb7948bf33b804733fc3814253c4b906980fa988
uzair1707 <51264502+uzair1707@users.noreply.github.com>
2023-06-25 20:24:09 +0500

    Update Testimonial.js
    Updated the alignment of testimonial section

--- components/HomeClone/Testimonial.js
+++ components/HomeClone/Testimonial.js
@@ -31,28 +31,26 @@ export default function Example() {
             Testimonials
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-       What our customers are saying
+            What our customers are saying
           </p>
         </div>
         <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
-          <div className="-mt-8 sm:-mx-4 sm:columns-3 lg:columns-3 sm:text-[0] ">
+          <div className="-mt-8 sm:-mx-4 sm:columns-3 lg:columns-3 sm:text-[0]">
             {testimonials.map((testimonial) => (
               <div
                 key={testimonial.author.handle}
                 className="pt-8 sm:w-[100%] sm:px-4"
               >
-                <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
-                  <figcaption className="mt-6 flex items-center gap-x-4">
-                    <div>
-                      <div className="font-semibold text-gray-900 text-lg">
-                        {testimonial.author.name}
-                      </div>
-                      {/* <div className="text-gray-600">{`@${testimonial.author.handle}`}</div> */}
-                    </div>
-                  </figcaption>
-                  <blockquote className="text-gray-900 my-4 md:my-5">
+                <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6 flex flex-col items-center">
+                  <blockquote className="text-gray-900 my-4 md:my-5 text-center">
                     <p>{`“${testimonial.body}”`}</p>
                   </blockquote>
+                  <figcaption className="mt-6 flex flex-col items-center">
+                    <div className="font-semibold text-gray-900 text-lg">
+                      {testimonial.author.name}
+                    </div>
+                    {/* <div className="text-gray-600">{`@${testimonial.author.handle}`}</div> */}
+                  </figcaption>
                 </figure>
               </div>
             ))}
